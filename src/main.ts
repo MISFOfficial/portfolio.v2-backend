6e2541efe77811c62a6763650a37d6eef6c3555e
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2026-02-16 20:12:48 +0600

    dfd

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function main() {
+  const port = process.env.PORT ?? 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log(`My Portfolio API is running at port ${port}`);
 }
-bootstrap();
+main();
