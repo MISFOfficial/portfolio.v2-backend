670e2b9275538131ee7eebd564d9334c25199f22
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2026-02-18 22:51:38 +0600

    l

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,6 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  @ApiTags("Home")
   getHello(): string {
     return this.appService.getHello();
   }
